cdb4c52f3be3b881ec7f9664e669a04229db352a
denicozma2019 <denis.cozma2015@gmail.com>
2023-03-04 09:05:31 +0200

    Initial commit

---
+++ src/test/java/Lab_1JAVA_EE/JavaEE/JavaEeApplicationTests.java
@@ -0,0 +1,13 @@
+package Lab_1JAVA_EE.JavaEE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
